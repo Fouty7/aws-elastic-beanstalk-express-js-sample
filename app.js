a8d5ca30d08e55a1a7c21c0654abe146490cc153
Aminu Ndakun <amin4712@gmail.com>
2023-03-09 12:42:24 +0100

    testing manual review

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing code pipeline and code build'));
+app.get('/', (req, res) => res.send('Testing Manual Review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
